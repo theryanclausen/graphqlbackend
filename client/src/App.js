a40fed89ee795853f4e92782944b7b1eb6e2060e
theryanclausen <theryanclausen@gmail.com>
2019-05-27 21:50:24 -0700

    react and apollo client thrown together

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import { onError } from "apollo-link-error";
+import { withClientState } from "apollo-link-state";
+import { ApolloLink, Observable } from "apollo-link";
+
+const client = new ApolloClient({
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          )
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    new HttpLink({ uri: "http://localhost:3339/graphql" })
+  ]),
+  cache: new InMemoryCache()
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div>Hi</div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const cors = require('cors')
 const expressGraphQL = require("express-graphql");
 
 const schema = require('./schema.js')
 const port = 3339;
 const app = express();
+
+app.use(cors())
 
 app.use(
   "/graphql",
